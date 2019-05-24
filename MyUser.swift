73d0a3a7689d7d50292940436429bbf78e2fa2cf
Sipos Péter <33089484+SiposPtr@users.noreply.github.com>
2019-05-24 23:16:34 +0200

    Add files via upload

---
+++ MyUser.swift
@@ -0,0 +1,16 @@
+//
+//  MyUser.swift
+//  SiposSocialMedia
+//
+//  Created by Sipos Péter on 2019. 05. 24..
+//  Copyright © 2019. Sipos Péter. All rights reserved.
+//
+
+import UIKit
+
+class MyUser: NSObject {
+    var email: String?
+    var isEmailVerified: String?
+    var sendEmailVerification: String?
+}
+
